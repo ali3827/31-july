c82592122a893bf3cf5b3f40ac2f06b62f5ddee9
Khaleel Khan <749217@student.tor.loc>
2019-07-31 15:57:10 -0400

    second commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,9 +1,11 @@
 const http = require('http');
 function handler(req, res){
+    const parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.pathname === '/'){
     res.writeHead(200, {'Content-type':'text/plain'});
 res.write('Hello, I am a webserver!');
-res.end();
-
+res.end();}
+else {res.writeHead(404,{'Content-type':'text/plain'} );res.end(); }
 
 }
 const server = http.createServer();
